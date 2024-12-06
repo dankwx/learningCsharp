054fb9f8b02d80ac4c42fbfa16fa1ac161d7f27b
Daniel Kondlatsch <danielkondlatsch.p@gmail.com>
2024-12-06 20:40:36 -0300

    Add project files.

---
+++ learningCsharp/Program.cs
@@ -0,0 +1,10 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+        string valorInput = Console.ReadLine() ?? string.Empty;
+        Console.WriteLine($"Valor Informado: {(!String.IsNullOrEmpty(valorInput) ? valorInput : "No data")}");
+        Console.ReadKey();
+    }
+}
